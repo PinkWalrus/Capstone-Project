4ce228baead3fdbe2c225600f9ed92fcc2338bc0
Loren <lorensprague@icloud.com>
2022-12-02 03:53:58 -0500

    update sessions#create and sessions#destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,23 @@
 class SessionsController < ApplicationController
     # /login
     def create
-        binding.pry
+        # try to find a user by email
+        user = User.find_by(email: params[:email])
+        # if we find a user && they are authenticated with password
+        if user&.authenticate(params[:password])
+            # set the session[:user_id] to the found and authorized user
+            session[:user_id] = user.id
+            # render user with status ok
+            render json: user, status: :ok
+        else
+            render json: { errors: ["Invalid email or password"] }, status: :unauthorized
+        end
     end
 
     # /logout
     def destroy
-        binding.pry
+        session.delete :user_id
+        head :no_content
     end
 
 end
