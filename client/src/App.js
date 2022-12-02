25b0e80557ef2d25a64f51768c69d306e81a529d
Loren <lorensprague@icloud.com>
2022-12-02 05:48:07 -0500

    update App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,22 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Signup from "./components/Auth/Signup";
+import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
+import Signup from "./components/Auth/Signup";
+import Login from "./components/Login/Login";
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState({
+    first_name: "",
+  });
 
   return (
     <BrowserRouter>
       <div className="App">
+        <NavBar setUser={setUser} />
         <Routes>
           <Route path="/signup" element={<Signup setUser={setUser} />} />
+          <Route path="/login" element={<Login setUser={setUser} />} />
           <Route path="/testing" element={<h1>Test Route</h1>} />
           <Route path="/" element={<Home user={user} />} />
         </Routes>
