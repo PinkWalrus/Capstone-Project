29eedfa65ba4305497a7b1ad0eb78af7a4c29da9
Loren <lorensprague@icloud.com>
2022-12-06 02:34:56 -0500

    update users#show and add fetch to /me

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable
+
+    def render_unprocessable(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
 
     def hello_world
         session[:count] = (session[:count] || 0) + 1
