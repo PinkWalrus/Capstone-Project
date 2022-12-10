21c02cc4e5638084436164478750fc939084933b
Loren <lorensprague@icloud.com>
2022-12-10 11:46:23 -0500

    create ProductModal component

---
+++ client/src/components/ProductModal/ProductModal.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+function ProductModal() {
+  const [products, setProducts] = useState([]);
+  //   let params = useParams();
+  let { id } = useParams();
+
+  useEffect(() => {
+    fetch(`/products/${id}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        setProducts([data]);
+      });
+  }, []);
+
+  return (
+    <div>
+      {/* <Category products={products} /> */}
+      <div>
+        {products.map((product) => {
+          return (
+            <div key={product.id}>
+              <h2>{product.name}</h2>
+              <p>{product.category}</p>
+              <p>{product.description}</p>
+              <p>{product.price}</p>
+              <img src={product.product_image} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default ProductModal;
