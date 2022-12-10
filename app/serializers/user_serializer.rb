b53997e6525aeddcac4936f9e43cdbd73d574594
Loren <lorensprague@icloud.com>
2022-12-10 11:27:45 -0500

    update User Serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :first_name, :last_name, :address_line1, :address_line2, :city, :state, :zipcode, :phone_number
+
+  has_one :cart
+  # has_many :invoices
+
+  # def cart
+  #   {id: 1}
+  # end
+
 end
