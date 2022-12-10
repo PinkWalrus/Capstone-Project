732b634090971faa011b6f9fe84bc2bf48605b77
Loren <lorensprague@icloud.com>
2022-12-10 11:20:36 -0500

    update Carts controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,36 @@
 class CartsController < ApplicationController
+    
+    def index
+        carts = Cart.all
+        render json: carts, status: :ok 
+     end
+
+     def create
+        cart = Cart.create(cart_params)
+        render json: cart, status: :created
+     end
+ 
+     def show
+         cart = Cart.find_by(id: params[:id])
+         render json: cart, status: :ok
+     end
+
+    def update
+        cart = Cart.find_by(id: params[:id])
+        cart.update(cart_params)
+        render json: cart, status: :ok
+    end
+
+    def destroy
+        cart = Cart.find_by(id: params[:id])
+        cart.destroy
+        head :no_content
+    end
+
+    private
+
+    def cart_params
+        params.permit(:quantity, :cart_total)
+    end
+
 end
