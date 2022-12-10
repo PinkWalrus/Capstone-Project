d3225eec3cb4f032ad2931aa2ee49b981610a954
Loren <lorensprague@icloud.com>
2022-12-10 11:49:24 -0500

    update Category component

--- client/src/components/Category/Category.js
+++ client/src/components/Category/Category.js
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import FilterCard from "../FilterCard/FilterCard";
+// import FilterCard from "../FilterCard/FilterCard";
+import FilterCategory from "../FilterCategory/FilterCategory";
+import ProductCard from "../ProductCard/ProductCard";
 
-function Category() {
-  return <div>Category</div>;
+function Category({ products }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  // function onCategoryChange(category) {
+  //   setSelectedCategory(category);
+  // }
+
+  const productsToDisplay = products.filter(
+    (product) =>
+      selectedCategory === "All" || product.category === selectedCategory
+  );
+
+  products
+    .map((product) => product.category)
+    .filter((category, index) => category.indexOf(category) === index);
+
+  return (
+    <div>
+      {/* <div className="product-list-grid-container">
+        {products.map((product) => {
+          return <div key={product.id}>{product.category}</div>;
+        })}
+      </div> */}
+      {/* <button
+        type="submit"
+        value="All"
+        onClick={(e) => onCategoryChange(e.target.value)}
+      >
+        All
+      </button> */}
+      {/* <div>
+        {productsToDisplay.map((product) => (
+          <FilterCategory
+            key={product.id}
+            product={product}
+            onCategoryChange={onCategoryChange}
+          />
+        ))}
+      </div> */}
+      <FilterCategory
+        // categories={["All", ...categories]}
+        categories={["All", "Light Roast", "Medium Roast", "Dark Roast"]}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
+      <FilterCard productsToDisplay={productsToDisplay} />
+    </div>
+  );
 }
 
 export default Category;
