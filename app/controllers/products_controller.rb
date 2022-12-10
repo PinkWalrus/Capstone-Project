bf7d55a8b37b00602d5d554a26c123afcd095511
Loren <lorensprague@icloud.com>
2022-12-10 11:22:09 -0500

    update Products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,24 @@
 class ProductsController < ApplicationController
     def index
-       products = Product.all
-       render json: products 
+    #    products = Product.all
+    #    render json: products, status: :ok 
+        render json: Product.all, status: :ok
     end
 
     def show
+        # product = Product.find(params[:id])
+        # render json: product, status: :ok
         product = Product.find_by(id: params[:id])
-        render json: product
+            if product
+                render json: product, status: :ok
+            else
+                render json: { errors: ["Product not found."] }, status: :not_found
+            end
+    end
+
+    private
+
+    def product_params
+        params.permit(:name, :description, :product_image, :price, :quantity_in_stock)
     end
 end
