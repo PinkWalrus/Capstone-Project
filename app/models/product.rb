e78dea658b9cac28e289d8dc553bc512e95fa95e
Loren <lorensprague@icloud.com>
2022-12-13 19:03:23 -0500

    update Cart and Product models with dependent destroy

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
-    has_many :product_carts
+    has_many :product_carts, dependent: :destroy
     has_many :carts, through: :product_carts
 
-    # has_many :invoices
+    has_many :invoices
 end
