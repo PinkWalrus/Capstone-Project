321706a0f921c7d4fbb9910a5238bc619a8605ec
Loren <lorensprague@icloud.com>
2022-12-14 18:33:18 -0500

    update Cart#update and Cart#destroy

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -18,7 +18,9 @@ class CartsController < ApplicationController
      end
 
     def update
-        cart = Cart.find_by(id: params[:id])
+        # cart = Cart.find_by(id: params[:id])
+        cart = Cart.find_by(id: session[:current_cart_id])
+        user = User.find(session[:user_id])
         # binding.pry
         if params[:type] == "DELETE_PRODUCT"
             # product = Product.find(params[:payload][:id])
@@ -29,14 +31,19 @@ class CartsController < ApplicationController
             # render json
         end
         # cart.update(cart_params)
-        render json: cart, status: :ok
+        render json: user, status: :ok
     end
 
     def destroy
-        cart = Cart.find_by(id: params[:id])
+        cart = Cart.find_by(id: session[:current_cart_id])
+        user = User.find(session[:user_id])
+        # binding.pry
         # cart = Cart.find_by(id: session[:current_cart_id])
         cart.destroy
-        head :no_content
+        new_cart = Cart.create
+        user.cart = new_cart
+        session[:current_cart_id] = new_cart.id
+        render json: user, status: :ok
     end
 
     private
