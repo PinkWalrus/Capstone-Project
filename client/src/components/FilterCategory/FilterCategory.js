1b30f8cdd62366b0dadb7f2972afa115bd46b2d1
Loren <lorensprague@icloud.com>
2022-12-20 18:49:21 -0500

    add styling to FilterCategory component

--- client/src/components/FilterCategory/FilterCategory.js
+++ client/src/components/FilterCategory/FilterCategory.js
@@ -1,16 +1,20 @@
 import React from "react";
+import "./FilterCategory.css";
 
 function FilterCategory({ categories, setSelectedCategory }) {
   return (
-    <div>
+    <ul className="filter-category">
       {categories.map((category) => (
-        <div key={category}>
-          <button onClick={() => setSelectedCategory(category)}>
+        <li key={category} className="filter-category">
+          <button
+            className="filter-category"
+            onClick={() => setSelectedCategory(category)}
+          >
             {category}
           </button>
-        </div>
+        </li>
       ))}
-    </div>
+    </ul>
   );
 }
 
