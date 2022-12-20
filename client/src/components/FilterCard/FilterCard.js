98aae6eb533bfca3a2e49897418eb69c5c59cafc
Loren <lorensprague@icloud.com>
2022-12-20 18:44:58 -0500

    add responsive grid to FilterCard component

--- client/src/components/FilterCard/FilterCard.js
+++ client/src/components/FilterCard/FilterCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
 import ProductCard from "../ProductCard/ProductCard";
 
 function FilterCard({
@@ -8,17 +9,21 @@ function FilterCard({
   addToCartClick,
 }) {
   return (
-    <div className="product-list-grid-container">
-      {productsToDisplay.map((product) => (
-        <ProductCard
-          key={product.id}
-          product={product}
-          setCartItems={setCartItems}
-          addProduct={addProduct}
-          addToCartClick={addToCartClick}
-        />
-      ))}
-    </div>
+    <>
+      <Row xs={1} sm={2} lg={3} xl={4} className="g-3">
+        {productsToDisplay.map((product) => (
+          <Col>
+            <ProductCard
+              key={product.id}
+              product={product}
+              setCartItems={setCartItems}
+              addProduct={addProduct}
+              addToCartClick={addToCartClick}
+            />
+          </Col>
+        ))}
+      </Row>
+    </>
   );
 }
 
