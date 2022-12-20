68b50c6fed77e321fbdf0312c39f48bb2500f5f5
Loren <lorensprague@icloud.com>
2022-12-20 18:50:08 -0500

    remove comments and update imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import { useState, useEffect, useContext } from "react";
-import { Routes, Route, useParams, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { UserContext } from "./context/UserProvider";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./pages/Home/Home";
-import Signup from "./components/Auth/Signup/Signup";
-import Login from "./components/Auth/Login/Login";
+import Signup from "./pages/Auth/Signup/Signup";
+import Login from "./pages/Auth/Login/Login";
 import ProductList from "./pages/ProductList/ProductList";
 import ProductModal from "./components/ProductModal/ProductModal";
 import Account from "./pages/Account/Account";
@@ -14,7 +14,6 @@ import Invoices from "./pages/Invoices/Invoices";
 function App() {
   const [products, setProducts] = useState([]);
   const [cartItems, setCartItems] = useState([]);
-  let { id } = useParams();
 
   const { user, setUser, errors, setErrors } = useContext(UserContext);
 
@@ -22,33 +21,9 @@ function App() {
     fetch("/products")
       .then((resp) => resp.json())
       .then((data) => {
-        // console.log(data);
         setProducts([...data]);
       });
   }, []);
-
-  useEffect(() => {
-    fetch(`/users/${id}`)
-      .then((resp) => resp.json())
-      .then((data) => {
-        // console.log([...data.cart.products]);
-        // console.log(data.cart.products[0].quantity);
-        // setCartItems([...data.cart.products]);
-        // console.log([...data.cart.products]);
-        // debugger;
-      });
-  }, []);
-
-  // useEffect(() => {
-  //   fetch(`/carts/${user.cart.id}`)
-  //     .then((r) => r.json())
-  //     .then((data) => console.log(data));
-  // }, []);
-
-  // function addProduct(newProduct) {
-  //   console.log(newProduct);
-  //   setCartItems(newProduct);
-  // }
 
   const addToCartClick = (product) => {
     console.log(product);
@@ -79,13 +54,7 @@ function App() {
       <Routes>
         <Route
           path="/"
-          element={
-            <Home
-              products={products}
-              // addProduct={addProduct}
-              addToCartClick={addToCartClick}
-            />
-          }
+          element={<Home products={products} addToCartClick={addToCartClick} />}
         />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
@@ -95,7 +64,6 @@ function App() {
             <ProductList
               products={products}
               setCartItems={setCartItems}
-              // addProduct={addProduct}
               addToCartClick={addToCartClick}
             />
           }
