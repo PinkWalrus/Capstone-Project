42fe7bbb2d8d444595888dc3cdcfb21367c57263
Loren <lorensprague@icloud.com>
2023-01-06 15:10:40 -0500

    add product modal

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -1,33 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { MDBIcon } from "mdb-react-ui-kit";
+import { Container, Button, Col, Row, Form } from "react-bootstrap";
+import "./ProductCard.css";
+import {
+  MDBIcon,
+  MDBBtn,
+  MDBModal,
+  MDBModalDialog,
+  MDBModalContent,
+  MDBModalHeader,
+  MDBModalBody,
+  MDBModalFooter,
+} from "mdb-react-ui-kit";
 import "./ProductCard.css";
 
 function ProductCard({ product, addToCartClick }) {
   const { id, name, description, product_image, price } = product;
+  const [centredModal, setCentredModal] = useState(false);
+
+  const toggleShow = () => setCentredModal(!centredModal);
 
   return (
     <div>
-      <Link to={`/products/${id}`}>
-        <div className="product-card-container">
-          <img src={product_image} />
-          <div className="product-card-info">
-            <h4 className="product-card-title">{name}</h4>
-            <hr className="product-card-hr" />
-            <span className="product-card-description">{description}</span>
-            <Link as="li" onClick={() => addToCartClick(product)}>
-              <div>
-                <button className="product-card-cart">
-                  <MDBIcon
-                    className="product-card-cart"
-                    icon="shopping-cart"
-                  ></MDBIcon>
-                </button>
-              </div>
-            </Link>
-          </div>
+      {/* <Link to={`/products/${id}`}> */}
+      {/* <MDBBtn onClick={toggleShow}>Vertically centered modal</MDBBtn> */}
+
+      <MDBModal tabIndex="-1" show={centredModal} setShow={setCentredModal}>
+        <MDBModalDialog centered size="xl">
+          <MDBModalContent>
+            <MDBModalHeader>
+              <MDBBtn
+                className="btn-close"
+                color="none"
+                onClick={toggleShow}
+              ></MDBBtn>
+            </MDBModalHeader>
+            <MDBModalBody style={{ display: "flex", paddingLeft: "4em" }}>
+              <Col>
+                <Row>
+                  <h2>{product.name}</h2>
+                  <hr></hr>
+                  <p>{product.category}</p>
+                  <p>{product.description}</p>
+                  <span>
+                    <strong>{product.price}</strong>
+                    <Link as="li" onClick={() => addToCartClick(product)}>
+                      <button className="add-to-cart">ADD TO CART</button>
+                    </Link>
+                  </span>
+                </Row>
+              </Col>
+              <Col>
+                <Row>
+                  <img src={product.product_image} />
+                </Row>
+              </Col>
+            </MDBModalBody>
+          </MDBModalContent>
+        </MDBModalDialog>
+      </MDBModal>
+      <div onClick={toggleShow} className="product-card-container">
+        <img src={product_image} />
+        <div className="product-card-info">
+          <h4 className="product-card-title">{name}</h4>
+          <hr className="product-card-hr" />
+          <span className="product-card-description">{description}</span>
+          <Link as="li" onClick={() => addToCartClick(product)}>
+            <div>
+              <button className="product-card-cart">
+                <MDBIcon
+                  className="product-card-cart"
+                  icon="shopping-cart"
+                ></MDBIcon>
+              </button>
+            </div>
+          </Link>
         </div>
-      </Link>
+      </div>
+      {/* </Link> */}
     </div>
   );
 }
